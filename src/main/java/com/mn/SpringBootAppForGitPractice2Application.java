bacf62898d2145c674b6e12dc6b1b0b3f8ca7ede
TL <TL@gmail.com>
2020-06-10 14:30:23 +0530

    creating structure

---
+++ src/main/java/com/mn/SpringBootAppForGitPractice2Application.java
@@ -0,0 +1,13 @@
+package com.mn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppForGitPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppForGitPractice2Application.class, args);
+	}
+
+}
